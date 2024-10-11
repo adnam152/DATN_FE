2e66847b6f73ba17f3bb45de947ba3e6875cb204
Tuấn Nam <teddy.nam152@gmail.com>
2024-10-11 22:14:17 +0700

    fetch data home

--- src/public/pages/Home/Home.jsx
+++ src/public/pages/Home/Home.jsx
@@ -1,11 +1,39 @@
-import ListProduct from "../../components/ListProduct/ListProduct"
+import { useEffect, useState } from "react"
+import ListProduct from "../../components/ListProduct/ListProduct";
+import useProduct from "../../../hooks/useProduct";
 
 function HomePage() {
-    const products = [1,2,3,4,5];
+    const [newProducts, setNewProducts] = useState([]);
+    const [trendProducts, setTrendProducts] = useState([]);
+    const { getProducts } = useProduct();
+
+    useEffect(() => {
+        // Get new products
+        getProducts()
+            .then(response => {
+                setNewProducts(response.data);
+            })
+            .catch(error => {
+                console.error(error);
+            });
+
+        // Get trending products
+        getProducts()
+            .then(response => {
+                setTrendProducts(response.data);
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    }, []);
 
     return (
         <>
-            <ListProduct products={products}/>
+            {/* New arrival */}
+            <ListProduct products={newProducts} />
+
+            {/* Trending */}
+            <ListProduct products={trendProducts} />
         </>
     )
 }
