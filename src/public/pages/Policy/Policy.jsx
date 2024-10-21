9b077c08f4914c7d39f9aadb1ffbf86d9a941d66
Tuấn Nam <teddy.nam152@gmail.com>
2024-10-21 20:32:04 +0700

    Update Policy

--- src/public/pages/Policy/Policy.jsx
+++ src/public/pages/Policy/Policy.jsx
@@ -1,6 +1,6 @@
 function Policy() {
     return (
-        <div className="bg-white rounded-lg  p-6 m-5 w-full max-w-7xl mx-auto" style={{ boxShadow: '0 15px 25px rgba(0, 0, 0, 0.3)' }}>
+        <div className="bg-white rounded-lg p-6 m-5 w-full max-w-7xl mx-auto">
             <div className="w-full border-b border-gray-300">
                 <div className="grid grid-cols-2 md:grid-cols-4 gap-4 py-2 container mx-auto">
                     <div className="flex items-center space-x-2 justify-center">
@@ -22,82 +22,63 @@ function Policy() {
                 </div>
 
             </div>
-            <div className="mt-4">
+            <div className="mt-4 px-5">
                 <h1 className="text-2xl font-bold">CHÍNH SÁCH BẢO MẬT THÔNG TIN VÀ QUYỀN RIÊNG TƯ</h1>
-                <div className="mt-4">
-                    <h2 className="font-bold">1- Chính sách Bảo vệ thông tin cá nhân</h2>
-                    <p>Nhằm đảm bảo an toàn cho website và bảo mật thông tin cho người tiêu dùng Chúng tôi đưa ra một số chính sách bảo mật thông tin cho khách hàng cá nhân và tổ chức khi mua hàng tại website: <a href="" className="font-semibold">FreeStyle Shop</a></p>
-                    <br></br>
-                    <p>Chính sách bảo mật sẽ giải thích cách chúng tôi tiếp nhận, sử dụng và (trong trường hợp nào đó) tiết lộ thông tin cá nhân của bạn. Chính sách cũng sẽ giải thích các bước chúng tôi thực hiện để bảo mật thông tin cá nhân của khách hàng. Cuối cùng, chính sách bảo mật sẽ giải thích quyền lựa chọn của bạn về việc thu thập, sử dụng và tiết lộ thông tin cá nhân của mình.</p>
-                    <br></br>
-                    <p>Bảo vệ dữ liệu cá nhân và gây dựng được niềm tin cho bạn là vấn đề rất quan trọng với công ty. Vì vậy, chúng tôi sẽ dùng tên và các thông tin khác liên quan đến bạn tuân thủ theo nội dung của chính sách bảo mật. Chúng tôi chỉ thu thập những thông tin cần thiết liên quan đến giao dịch mua bán.</p>
-                </div>
-                <div className="mt-4">
-                    <h2 className="font-bold">2. Mục đích thu thập thông tin:</h2>
-                    <p>Việc thu thập dữ liệu của website sẽ giúp chúng tôi:</p>
-                    <br />
-                    <p>+ Nắm bắt được các yêu cầu, mong muốn của khách hàng nhằm nâng cao chất lượng sản phẩm.</p>
-                    <p>+ Giúp khách hàng cập nhật các thông tin chương trình khuyến mại, giảm giá do chúng tôi tổ chức sớm nhất.</p>
-                    <p>+ Hỗ trợ khách hàng khi có khiếu nại, ý kiến một cách nhanh nhất.</p>
-                    <br />
-                    <h2 className="font-bold">Phạm vi thu thập thông tin:</h2>
-                    <br />
-                    <p>+ Họ và Tên</p>
-                    <br />
-                    <p>+ Địa Chỉ</p>
-                    <br />
-                    <p>+ Email</p>
-                    <br />
-                    <p>+ Fax</p>
-                    <br />
-                    <p>+ Điẹn Thoại</p>
-                    <br />
-                    <p>+ Nội dung cần liên hệ</p>
-                </div>
-                <div className="mt-4">
-                    <h2 className="font-bold">3 - Phạm vi sử dụng thông tin:</h2>
-                    <p>Thông tin thu thập được chúng tôi sử dụng cho các mục đích sau:</p>
-                    <br />
-                    <p>+ Giao hàng cho quý khách đã mua hàng tại: <a href="" className="font-semibold">FreeStyle Shop</a></p>
-                    <br />
-                    <p>+ Thông báo về việc giao hàng và hỗ trợ cho khách hàng</p>
-                    <br />
-                    <p>+ Xử lý các đơn đặt hàng và cung cấp sản phẩm thông qua website: <a href="" className="font-semibold">FreeStyle Shop</a></p>
-                    <br />
-                    <p>Ngoài ra, các thông tin giao dịch gồm: lịch sử mua hàng, giá trị giao dịch, phương thức vận chuyển và thanh toán cũng được website: <a href="" className="font-semibold">FreeStyle Shop</a> lưu trữ nhằm giải quyết những vấn đề có thể phát sinh về sau.</p>
-                    <h2 className="font-bold">Chúng tôi sẽ không chia sẻ thông tin khách hàng trừ những trường hợp cụ thể sau đây:</h2>
-                    <p>+ Để bảo vệ <a href="" className="font-semibold">FreeStyle Shop</a> và các bên thứ ba khác: Chúng tôi chỉ đưa ra thông tin tài khoản và những thông tin cá nhân khác khi tin chắc rằng việc đưa những thông tin đó là phù hợp với luật pháp, bảo vệ quyền lợi, tài sản của người sử dụng dịch vụ, của <a href="" className="font-semibold">FreeStyle Shop</a> và các bên thứ ba khác.</p>
-                    <p>+ Theo yêu cầu pháp l‎ý từ một cơ quan chính phủ hoặc khi chúng tôi tin rằng việc làm đó là cần thiết và phù hợp nhằm tuân theo các yêu cầu pháp l‎ý Nó chắc chắn không bao gồm việc bán, chia sẻ dẫn đến việc làm lộ thông tin cá nhân của khách hàng vì mục đích thương mại vi phạm những cam kết được đặt ra trong quy định Chính sách bảo mật thông tin khách hàng của chúng tôi.</p>
-                </div>
-                <div className="mt-4">
-                    <h2 className="font-bold">4 - Thời gian lưu trữ thông tin:</h2>
-                    <p>Chúng tôi sẽ lưu trữ các thông tin cá nhân do khách hàng cung cấp trên các hệ thống nội bộ của chúng tôi trong quá trình cung cấp sản phẩm cho khách hàng, cho đến khi hoàn thành mục đích thu thập, hoặc khi khách hàng có yêu cầu hủy các thông tin đã cung cấp.</p>
-                    <br />
-                    <h2 className="font-bold">Cam kết bảo mật thông tin cá nhân khách hàng:</h2>
-                    <p>Công ty cam kết bảo đảm an toàn thông tin cho quý khách hàng khi đăng ký thông tin cá nhân với chúng tôi. Chúng tôi cam kết không trao đổi mua bán thông tin khách hàng vì mục đích thương mại. Mọi sự chia sẻ và sử dụng thông tin khách hàng chúng tôi cam kết thực hiện theo chính sách bảo mật của chúng tôi.</p>
-                    <br />
-                    <p>Chúng tôi chỉ sử dụng thông tin khách hàng vào những việc hoàn toàn chính đáng, được sự đồng ý chấp thuận của khách hàng. Tuyệt đối không cung cấp thông tin cho một bên thứ 3 nếu không được khách hàng đồng ý. Chỉ cung cấp thông tin của khách hàng khi nó có lợi với khách hàng và được sự cho phép của khách hàng. Nhưng chúng tôi vẫn sẻ phải cung cấp thông tin cho bên thứ 3 trong một số trường hợp sau:</p>
-                    <br />
-                    <p>- Được sự cho phép của khách hàng</p>
-                    <br />
-                    <p>- Theo yêu cầu của các tổ chức, cơ quan, chính phủ, nhà nước khi xác nhận được việc làm đó là hợp pháp và không vi phạm pháp luật nhà nước Việt Nam</p>
-                    <br />
-                    <h2 className="font-bold">Địa chỉ của đơn vị thu thập và quản lý thông tin cá nhân</h2>
-                    <br />
-                    <p>Tên đơn vị: CÔNG TY CỔ PHẦN ILEAD</p>
-                    <br />
-                    <p>Giấy chứng nhận đăng ký kinh doanh số: 0311951294</p>
-                    <br />
-                    <p>Ngày cấp: 14 tháng 11 năm 2024</p>
-                    <br />
-                    <p>Địa chỉ trụ sở: Số nhà 35, ngách 212/14 đường Phú Mỹ, Mỹ Đình 2, Nam Từ Liêm, Hà Nội</p>
-                    <br />
-                    <p>Điện thoại: (+84) 357 420 766</p>
-                    <br />
-                    <p>Phương tiện và công cụ để người dùng tiếp cận và chỉnh sửa dữ liệu cá nhân của mình. Khách hàng có thể thực hiện các quyền trên bằng cách liên hệ với chúng tôi qua số điện thoại hoặc địa chỉ liên lạc được công bố trên website: </p>
-                    <br />
-                    <h2 className="font-bold text-center">Chúng tôi cam kết sẽ khiến quý khách cảm thấy tin tưởng và hài lòng về bảo mật thông tin cá nhân khi tham gia và sử dụng website của công ty</h2>
-                </div>
+                <ol style={{ listStyleType: 'auto', listStylePosition: 'inside' }}>
+                    <li className="font-bold mt-4">Chính sách Bảo vệ thông tin cá nhân:</li>
+                    <div className="mt-2">
+                        <p>Nhằm đảm bảo an toàn cho website và bảo mật thông tin cho người tiêu dùng Chúng tôi đưa ra một số chính sách bảo mật thông tin cho khách hàng cá nhân và tổ chức khi mua hàng tại website: <a href="" className="font-semibold">FreeStyle Shop</a></p>
+                        <br></br>
+                        <p>Chính sách bảo mật sẽ giải thích cách chúng tôi tiếp nhận, sử dụng và (trong trường hợp nào đó) tiết lộ thông tin cá nhân của bạn. Chính sách cũng sẽ giải thích các bước chúng tôi thực hiện để bảo mật thông tin cá nhân của khách hàng. Cuối cùng, chính sách bảo mật sẽ giải thích quyền lựa chọn của bạn về việc thu thập, sử dụng và tiết lộ thông tin cá nhân của mình.</p>
+                        <br></br>
+                        <p>Bảo vệ dữ liệu cá nhân và gây dựng được niềm tin cho bạn là vấn đề rất quan trọng với công ty. Vì vậy, chúng tôi sẽ dùng tên và các thông tin khác liên quan đến bạn tuân thủ theo nội dung của chính sách bảo mật. Chúng tôi chỉ thu thập những thông tin cần thiết liên quan đến giao dịch mua bán.</p>
+                    </div>
+
+                    <li className="font-bold mt-4">Mục đích thu thập thông tin:</li>
+                    <div className="mt-2">
+                        <p>Việc thu thập dữ liệu của website sẽ giúp chúng tôi:</p>
+                        <p>+ Nắm bắt được các yêu cầu, mong muốn của khách hàng nhằm nâng cao chất lượng sản phẩm.</p>
+                        <p>+ Giúp khách hàng cập nhật các thông tin chương trình khuyến mại, giảm giá do chúng tôi tổ chức sớm nhất.</p>
+                        <p>+ Hỗ trợ khách hàng khi có khiếu nại, ý kiến một cách nhanh nhất.</p>
+                        <h2 className="font-bold mt-2">Phạm vi thu thập thông tin:</h2>
+                        <p>+ Họ và Tên</p>
+                        <p>+ Địa Chỉ</p>
+                        <p>+ Email</p>
+                        <p>+ Fax</p>
+                        <p>+ Điẹn Thoại</p>
+                        <p>+ Nội dung cần liên hệ</p>
+                    </div>
+
+                    <li className="font-bold mt-4">Phạm vi sử dụng thông tin:</li>
+                    <div className="mt-2">
+                        <p>Thông tin thu thập được chúng tôi sử dụng cho các mục đích sau:</p>
+                        <p>+ Giao hàng cho quý khách đã mua hàng tại: <a href="" className="font-semibold">FreeStyle Shop</a></p>
+                        <p>+ Thông báo về việc giao hàng và hỗ trợ cho khách hàng</p>
+                        <p>+ Xử lý các đơn đặt hàng và cung cấp sản phẩm thông qua website: <a href="" className="font-semibold">FreeStyle Shop</a></p>
+                        <p>Ngoài ra, các thông tin giao dịch gồm: lịch sử mua hàng, giá trị giao dịch, phương thức vận chuyển và thanh toán cũng được website: <a href="" className="font-semibold">FreeStyle Shop</a> lưu trữ nhằm giải quyết những vấn đề có thể phát sinh về sau.</p>
+                        <h2 className="font-bold mt-2">Chúng tôi sẽ không chia sẻ thông tin khách hàng trừ những trường hợp cụ thể sau đây:</h2>
+                        <p>+ Để bảo vệ <a href="" className="font-semibold">FreeStyle Shop</a> và các bên thứ ba khác: Chúng tôi chỉ đưa ra thông tin tài khoản và những thông tin cá nhân khác khi tin chắc rằng việc đưa những thông tin đó là phù hợp với luật pháp, bảo vệ quyền lợi, tài sản của người sử dụng dịch vụ, của <a href="" className="font-semibold">FreeStyle Shop</a> và các bên thứ ba khác.</p>
+                        <p>+ Theo yêu cầu pháp l‎ý từ một cơ quan chính phủ hoặc khi chúng tôi tin rằng việc làm đó là cần thiết và phù hợp nhằm tuân theo các yêu cầu pháp l‎ý Nó chắc chắn không bao gồm việc bán, chia sẻ dẫn đến việc làm lộ thông tin cá nhân của khách hàng vì mục đích thương mại vi phạm những cam kết được đặt ra trong quy định Chính sách bảo mật thông tin khách hàng của chúng tôi.</p>
+                    </div>
+
+                    <li className="font-bold mt-4">Thời gian lưu trữ thông tin:</li>
+                    <div className="mt-2">
+                        <p>Chúng tôi sẽ lưu trữ các thông tin cá nhân do khách hàng cung cấp trên các hệ thống nội bộ của chúng tôi trong quá trình cung cấp sản phẩm cho khách hàng, cho đến khi hoàn thành mục đích thu thập, hoặc khi khách hàng có yêu cầu hủy các thông tin đã cung cấp.</p>
+                        <h2 className="font-bold mt-2">Cam kết bảo mật thông tin cá nhân khách hàng:</h2>
+                        <p>Công ty cam kết bảo đảm an toàn thông tin cho quý khách hàng khi đăng ký thông tin cá nhân với chúng tôi. Chúng tôi cam kết không trao đổi mua bán thông tin khách hàng vì mục đích thương mại. Mọi sự chia sẻ và sử dụng thông tin khách hàng chúng tôi cam kết thực hiện theo chính sách bảo mật của chúng tôi.</p>
+                        <p>Chúng tôi chỉ sử dụng thông tin khách hàng vào những việc hoàn toàn chính đáng, được sự đồng ý chấp thuận của khách hàng. Tuyệt đối không cung cấp thông tin cho một bên thứ 3 nếu không được khách hàng đồng ý. Chỉ cung cấp thông tin của khách hàng khi nó có lợi với khách hàng và được sự cho phép của khách hàng. Nhưng chúng tôi vẫn sẻ phải cung cấp thông tin cho bên thứ 3 trong một số trường hợp sau:</p>
+                        <p>- Được sự cho phép của khách hàng</p>
+                        <p>- Theo yêu cầu của các tổ chức, cơ quan, chính phủ, nhà nước khi xác nhận được việc làm đó là hợp pháp và không vi phạm pháp luật nhà nước Việt Nam</p>
+                        <h2 className="font-bold mt-2">Địa chỉ của đơn vị thu thập và quản lý thông tin cá nhân</h2>
+                        <p>Tên đơn vị: CÔNG TY CỔ PHẦN ILEAD</p>
+                        <p>Giấy chứng nhận đăng ký kinh doanh số: 0311951294</p>
+                        <p>Ngày cấp: 14 tháng 11 năm 2024</p>
+                        <p>Địa chỉ trụ sở: Số nhà 35, ngách 212/14 đường Phú Mỹ, Mỹ Đình 2, Nam Từ Liêm, Hà Nội</p>
+                        <p>Điện thoại: (+84) 357 420 766</p>
+                        <p>Phương tiện và công cụ để người dùng tiếp cận và chỉnh sửa dữ liệu cá nhân của mình. Khách hàng có thể thực hiện các quyền trên bằng cách liên hệ với chúng tôi qua số điện thoại hoặc địa chỉ liên lạc được công bố trên website: </p>
+                        <h2 className="font-bold text-center">Chúng tôi cam kết sẽ khiến quý khách cảm thấy tin tưởng và hài lòng về bảo mật thông tin cá nhân khi tham gia và sử dụng website của công ty</h2>
+                    </div>
+                </ol>
             </div>
         </div>
 
